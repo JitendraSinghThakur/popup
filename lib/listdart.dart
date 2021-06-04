46125df9065e42ef918b8afe7c4dec40c46da728
Jitendra Singh <jitendrasinghcs1993@gmail.com>
2021-06-04 19:27:23 +0530

    upload

---
+++ lib/listdart.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class MyListView extends StatefulWidget {
+  const MyListView({Key? key}) : super(key: key);
+
+  @override
+  MyListViewState createState() => MyListViewState();
+}
+
+class MyListViewState extends State<MyListView> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Sun'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Moon'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/jitendra.jpeg'),
+          ),
+          title: Text('Star'),
+          subtitle: Text("Some about its owen"),
+        ),
+      ],
+    );
+  }
+}
